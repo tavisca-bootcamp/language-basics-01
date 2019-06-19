466faca463a8c7e5e5d9417bb528e54a2a7b9643
Nikhil Prasad <nikhil.prasad@gmail.com>
2019-06-19 20:01:40 +0530

    Added starter code.

---
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test("42*47=1?74", 9);
+            Test("4?*47=1974", 2);
+            Test("42*?7=1974", 4);
+            Test("42*?47=1974", -1);
+            Test("2*12?=247", -1);
+        }
+
+        private static void Test(string args, int expected)
+        {
+            var result = FindDigit(args).Equals(expected) ? "PASS" : "FAIL";
+            Console.WriteLine($"{args} : {result}");
+        }
+
+        public static int FindDigit(string equation)
+        {
+            // Add your code here.
+            throw new NotImplementedException();
+        }
+    }
+}
